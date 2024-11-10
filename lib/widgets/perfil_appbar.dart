65d2878598bb7a86fa4d727426c2be7a8df1baf2
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-11-09 18:59:48 -0600

    Cambio en el appbar de perfil para desplegar información del usuario

--- lib/widgets/perfil_appbar.dart
+++ lib/widgets/perfil_appbar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sistema_cursos_front/pages/usuario/perfil_usuario_page.dart';
+import 'package:sistema_cursos_front/services/user_service.dart';
 
 class AppBarPerfil extends StatelessWidget implements PreferredSizeWidget {
   final bool arrow;
@@ -8,39 +10,39 @@ class AppBarPerfil extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final userService = Provider.of<UserService>(context);
+
     return AppBar(
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween, 
         children: [
           GestureDetector(
             onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const PerfilUsuarioPage(),
-                    ),
-                  );
-                },
+              Navigator.push
+              (
+                context,MaterialPageRoute(
+                  builder: (context) => const PerfilUsuarioPage(),
+                ),
+              );
+            },
             child: Row(
               children: [
-                Icon(
-                  size: 30,
-                  Icons.account_circle,
-                ),
+                const Icon( size: 30, Icons.account_circle, color: Color(0xFF14919B),),
                 const SizedBox(width: 8), 
-                const Text("Usuario"),
+                Text(userService.userProvider.name, style: const TextStyle(color: Color(0xFF14919B)),),
               ],
             ),
           ),
           // Flecha a la derecha
-          arrow
-              ? IconButton(
-                  onPressed: () {
-                    Navigator.pop(context); // Regresar a la página anterior
-                  },
-                  icon: const Icon(Icons.arrow_back),
-                )
-              : Container(), // Espacio vacío si arrow es falso
+          arrow ? 
+          IconButton(
+            onPressed: () {
+              Navigator.pop(context); // Regresar a la página anterior
+            },
+            icon: const Icon(Icons.arrow_back),
+          )
+          : Container(), // Espacio vacío si arrow es falso
         ],
       ),
     );
