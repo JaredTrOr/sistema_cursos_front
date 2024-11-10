b830364bf822a2e8456612ca2845eb793d9f4189
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-11-09 18:58:17 -0600

    Cambio en pantalla de cursos favoritos

--- lib/pages/usuario/cursos_adquiridos_page.dart
+++ lib/pages/usuario/cursos_adquiridos_page.dart
@@ -1,62 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sistema_cursos_front/services/courses_service.dart';
 import 'package:sistema_cursos_front/widgets/card_curso.dart';
+import 'package:sistema_cursos_front/widgets/no_courses.dart';
 // import '../widgets/card_curso.dart';
 
 //-----   Esta es la view de Favoritos  -----//
-
-List<Map<String, dynamic>> cursosAdquiridos = [
-  {
-    'title': 'Curso de Flutter',
-    'subtitle': 'Aprende a programar apps con Flutter',
-    'image':
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT7HZFCI0Euwzq8qotUFeCbHhhISYdqUt4rg&s',
-    'price': "100",
-  },
-  {
-    'title': 'Curso de Dart',
-    'subtitle': 'Aprende a programar con Dart',
-    'image':
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT7HZFCI0Euwzq8qotUFeCbHhhISYdqUt4rg&s',
-    'price': "50",
-  },
-];
 
 class CursosAdquiridosPage extends StatelessWidget {
   const CursosAdquiridosPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final courseService = Provider.of<CoursesService>(context);
 
     return Scaffold(
-      body: Column(children: [
-        SizedBox(
-          height: 40,
-        ),
-        Center(child: Text("Mis Favoritos")),
-        cursosAdquiridos.isNotEmpty ?
-        Expanded(
-          child: ListView.builder(
-            itemCount: cursosAdquiridos.length,
-            itemBuilder: (context, index) {
-              final curso = cursosAdquiridos[index];
-              return CardCurso(
-                title: curso['title']!,
-                subtitle: curso['subtitle']!,
-                image: curso['image']!,
-                price: curso['price']!,
-                onTap: () {
-                  // Aquí puedes agregar lógica para mostrar detalles del curso
-                },
-                onFavorite: () {
-                  // Aquí podrías implementar la lógica para eliminar de favoritos si lo deseas
-                },
-                isFavorite:
-                    true, // Siempre será verdadero ya que está en la lista de adquiridos
-              );
-            },
-          ),
-        ) : Container(),
-      ]),
-    );
+        body: courseService.courses.isNotEmpty
+            ? Column(
+                children: [
+                  const SizedBox(height: 40),
+                  const Center(child: Text("Mis Favoritos")),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: courseService.courses.length,
+                      itemBuilder: (context, index) {
+                        final course = courseService.courses[index];
+                        return CardCurso(
+                          course: course,
+                          onTap: () {
+                            // Aquí puedes agregar lógica para mostrar detalles del curso
+                          },
+                          onFavorite: () {
+                            // Aquí podrías implementar la lógica para eliminar de favoritos si lo deseas
+                          },
+                          isFavorite:
+                              true, // Siempre será verdadero ya que está en la lista de adquiridos
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              )
+            : const NoCourses(
+                description: 'No tienes cursos agregados a favoritos',
+              ));
   }
 }
