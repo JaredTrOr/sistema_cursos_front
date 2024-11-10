d71cff3d01738a1006720b661172c28466cdf2dd
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-11-09 18:55:17 -0600

    Agregación de providers en main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sistema_cursos_front/firebase_options.dart';
 import 'package:sistema_cursos_front/pages/creador_contenido/agregar_curso_page.dart';
 import 'package:sistema_cursos_front/pages/creador_contenido/cursos_creador_contenido_page.dart';
@@ -14,11 +15,28 @@ import 'package:sistema_cursos_front/pages/usuario/cursos_favoritos_page.dart';
 import 'package:sistema_cursos_front/pages/usuario/home_usuario_page.dart';
 import 'package:sistema_cursos_front/pages/usuario/metodo_pago_page.dart';
 import 'package:sistema_cursos_front/pages/usuario/perfil_usuario_page.dart';
+import 'package:sistema_cursos_front/services/courses_service.dart';
+import 'package:sistema_cursos_front/services/user_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(const AppState());
+}
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => UserService()),
+        ChangeNotifierProvider(create: (context) => CoursesService()),
+      ],
+      child: const MyApp(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
