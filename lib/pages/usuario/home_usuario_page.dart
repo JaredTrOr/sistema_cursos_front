5d414deaa3bcf7773c6293d7b6dbd84e00412920
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-11-09 23:39:32 -0600

    Corrección de despliegue de listado de cursos

--- lib/pages/usuario/home_usuario_page.dart
+++ lib/pages/usuario/home_usuario_page.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sistema_cursos_front/models/courses_model.dart';
 import 'package:sistema_cursos_front/services/courses_service.dart';
+import 'package:sistema_cursos_front/widgets/is_loading.dart';
 import 'package:sistema_cursos_front/widgets/no_courses.dart';
-import 'package:sistema_cursos_front/widgets/search_productos.dart';
-import 'package:sistema_cursos_front/pages/usuario/curso_page.dart';
 import 'package:sistema_cursos_front/widgets/card_curso.dart';
-import 'cursos_adquiridos_page.dart'; // Asegúrate de importar la página de cursos adquiridos
 
 class HomeUsuarioPage extends StatelessWidget {
   const HomeUsuarioPage({super.key});
@@ -16,37 +13,34 @@ class HomeUsuarioPage extends StatelessWidget {
     final courseService = Provider.of<CoursesService>(context);
 
     if (courseService.isLoading) {
-      return const Center(child: CircularProgressIndicator());
+      return const IsLoading();
     }    
 
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: 
-          courseService.courses.isNotEmpty ? 
-          ListView.builder(
-            itemCount: courseService.courses.length,
-            itemBuilder: (context, index) {
-              final course = courseService.courses[index];
-              return CardCurso(
-                course: course,
-                onTap: () {
-                          // Navigator.push(
-                          //   context,
-                          //   MaterialPageRoute(
-                          //     builder: (context) => CursoPage(course: course),
-                          //   ),
-                          // );
-                },
-                onFavorite: () {
-                  // Handle favorite action
-                },
-                isFavorite: false, // Set default favorite state
-              );
-            },
-          )
-          : const NoCourses(description: 'No hay cursos disponibles',),
-        ),
+      body: Expanded(
+        child: courseService.courses.isNotEmpty ? 
+        ListView.builder(
+          itemCount: courseService.courses.length,
+          itemBuilder: (context, index) {
+            final course = courseService.courses[index];
+            return CardCurso(
+              course: course,
+              onTap: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) => CursoPage(course: course),
+                        //   ),
+                        // );
+              },
+              onFavorite: () {
+                // Handle favorite action
+              },
+              isFavorite: false, // Set default favorite state
+            );
+          },
+        )
+        : const NoCourses(description: 'No hay cursos disponibles',),
       ),
     );
   }
