27154d3cbc07be1af9fd4112b4e8c641a12cf311
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-11-10 18:39:34 -0600

    Agregación de provider de favoritos y de carrito

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ import 'package:sistema_cursos_front/pages/usuario/cursos_favoritos_page.dart';
 import 'package:sistema_cursos_front/pages/usuario/home_usuario_page.dart';
 import 'package:sistema_cursos_front/pages/usuario/metodo_pago_page.dart';
 import 'package:sistema_cursos_front/pages/usuario/perfil_usuario_page.dart';
+import 'package:sistema_cursos_front/providers/cart_provider.dart';
+import 'package:sistema_cursos_front/providers/favorites_provider.dart';
 import 'package:sistema_cursos_front/services/courses_service.dart';
 import 'package:sistema_cursos_front/services/user_service.dart';
 
@@ -33,6 +35,8 @@ class AppState extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (context) => UserService()),
         ChangeNotifierProvider(create: (context) => CoursesService()),
+        ChangeNotifierProvider(create: (context) => CartProvider()),
+        ChangeNotifierProvider(create: (context) => FavoritesProvider())
       ],
       child: const MyApp(),
     );
@@ -58,7 +62,7 @@ class MyApp extends StatelessWidget {
         'navigation': (context) => const BarraNavegacion(),
         'cursos_adquiridos': (context) => const CursosAdquiridosPage(),
         'curso_favoritos':(context)=> const CursosFavoritosPage(),
-        'carrito_compras': (context) => CarritoComprasPage(),
+        'carrito_compras': (context) => const CarritoComprasPage(),
         'metodo_pago': (context) => const MetodoPagoPage(),
         'perfil_usuario': (context) => const PerfilUsuarioPage(),
 
