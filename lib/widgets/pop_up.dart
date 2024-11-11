dd1ddd46b7322d47e94f552aa47f4b0c4d3393dc
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-11-10 18:40:03 -0600

    Modificación de pop up para mensaje customizado emergente

--- lib/widgets/pop_up.dart
+++ lib/widgets/pop_up.dart
@@ -4,14 +4,34 @@ import 'package:flutter/material.dart';
 AwesomeDialog popUp({
   required BuildContext context,
   required String title,
-  required Widget body,
+  required String body,
+  required String dialogType,
   bool isHeader = true
 }) 
 {
   return AwesomeDialog(
     context: context,
-    dialogType: DialogType.warning,
+    dialogType: chooseDialogType(dialogType),
     title: title,
-    body: body,
+    body: Column(
+      children: [
+        const SizedBox(height: 10),
+        Text(title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        const SizedBox(height: 5),
+        Text(body, style: const TextStyle(fontSize: 15)),
+        const SizedBox(height: 20),
+      ],
+    ),
   )..show();
+}
+
+DialogType chooseDialogType(String type) {
+  switch (type) {
+    case 'warning': return DialogType.warning;
+    case 'error': return DialogType.error;
+    case 'success': return DialogType.success;
+    case 'info': return DialogType.info;
+    
+    default: return DialogType.info;
+  }
 }
