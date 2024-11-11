ea0f9850a6113724a5647e42763b264e466317b0
Jared Alexander Trujillo Ortiz <trujillojaredalexander@gmail.com>
2024-11-10 20:08:44 -0600

    Creación de pantalla de cursos favoritos

--- lib/pages/usuario/cursos_adquiridos_page.dart
+++ lib/pages/usuario/cursos_adquiridos_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sistema_cursos_front/services/courses_service.dart';
+import 'package:sistema_cursos_front/services/user_service.dart';
 import 'package:sistema_cursos_front/widgets/card_curso.dart';
 import 'package:sistema_cursos_front/widgets/no_courses.dart';
-// import '../widgets/card_curso.dart';
-
-//-----   Esta es la view de Favoritos  -----//
 
 class CursosAdquiridosPage extends StatelessWidget {
   const CursosAdquiridosPage({super.key});
@@ -13,28 +11,39 @@ class CursosAdquiridosPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final courseService = Provider.of<CoursesService>(context);
+    final userService = Provider.of<UserService>(context);
 
     return Scaffold(
-        body: courseService.courses.isNotEmpty
+        body: userService.getUserFavoriteCourses.isNotEmpty
             ? Column(
                 children: [
-                  const SizedBox(height: 40),
-                  const Center(child: Text("Mis Favoritos")),
+                  const SizedBox(height: 20),
+                  const Center(child: Text("Mis Favoritos", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)),
                   Expanded(
                     child: ListView.builder(
-                      itemCount: courseService.courses.length,
+                      itemCount: userService.getUserFavoriteCourses.length,
                       itemBuilder: (context, index) {
-                        final course = courseService.courses[index];
+
+                        String courseId = userService.getUserFavoriteCourses[index];
+                        final course = courseService.getCourseById(courseId);
+
                         return CardCurso(
                           course: course,
-                          onTap: () {
-                            // Aquí puedes agregar lógica para mostrar detalles del curso
+                          onAdd: () {
+                            
                           },
+                          onTap: () { },
                           onFavorite: () {
-                            // Aquí podrías implementar la lógica para eliminar de favoritos si lo deseas
+                            if (!userService.isFavoriteCourse(course.id!)) {
+                              userService.addFavoriteCourse(course.id!);
+                              courseService.increaseAmountOfFavorites(course);
+                              return;
+                            }
+                            userService.removeFavoriteCourse(course.id!);
+                            courseService.decreaseAmountOfFavorites(course);
                           },
-                          isFavorite:
-                              true, // Siempre será verdadero ya que está en la lista de adquiridos
+                          
+                          isFavorite: userService.isFavoriteCourse(course.id!), 
                         );
                       },
                     ),
